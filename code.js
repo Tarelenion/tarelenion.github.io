fe4488a0ec2de4cf5f95aa7ce2a50f0acd7f93e5
Tarelenion Narlúin <77344662+Tarelenion@users.noreply.github.com>
2021-04-03 21:31:58 +0200

    Update code.js

--- code.js
+++ code.js
@@ -2,3 +2,19 @@ function notready()
 {
   alert("This function is not yet ready!");
 }
+
+function pass()
+{
+  var id1 = "Byeongwoo";
+  var password1 = "rlaquddn";
+  var idinput = document.getElementById("ID").value;
+  var passwordinput = document.getElementById("password").value;
+  if(idinput == id1 && passwordinput == password1)
+    {
+      document.getElementById("authorisedornot").innerhtml = "Welcome, you have the Information allowence class 1.";
+    }
+  else
+    {
+      document.getElementById("authorisedornot").innerhtml="YOU ARE NOT AUTHORISED";
+    }
+}
